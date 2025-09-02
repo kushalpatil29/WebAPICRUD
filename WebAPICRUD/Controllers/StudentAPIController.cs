d2d4b111ed00639939551c66cc55ab8e6120aa0c
kushalpatil29 <kushalpatil.2004@gmail.com>
2025-09-02 10:40:47 +0530

    Add project files.

---
+++ WebAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPICRUD.Models;
+
+namespace WebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly CodeFirstContext context;
+        public StudentAPIController(CodeFirstContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentbyId(int id)
+        {
+            var stud = await context.Students.FindAsync(id);
+            if (stud == null)
+            {
+                return NotFound();
+            }
+            return stud;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id,Student std)
+        {
+           if(id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> deletestud(int id)
+        {
+            var s1 = await context.Students.FindAsync(id);
+            if(s1 == null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(s1);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPICRUD/Models/CodeFirstContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPICRUD.Models;
+
+public partial class CodeFirstContext : DbContext
+{
+
+    public CodeFirstContext(DbContextOptions<CodeFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Gender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Standard).HasColumnName("standard");
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    internal async Task FindAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
